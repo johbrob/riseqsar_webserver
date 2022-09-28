b03d19b0944e606122bceaa551997d7e2c07cfea
johbrob <johan.broberg92@gmail.com>
2022-09-28 15:07:44 +0200

    add run file

---
+++ webapp/run.py
@@ -0,0 +1,13 @@
+from webapp import create_app
+import argparse
+
+app = create_app()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='127.0.0.1')
+    parser.add_argument('--port', type=int, default=5002)
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+
+    app.run(host=args.host, port=args.port, debug=args.debug)
