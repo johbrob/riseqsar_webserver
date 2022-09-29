240071f99206eeffa2b972a38caeaf2781709ee2
johbrob <johan.broberg92@gmail.com>
2022-09-29 13:48:59 +0200

    change app start up

--- webapp/run.py
+++ webapp/run.py
@@ -5,9 +5,10 @@ app = create_app()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default='127.0.0.1')
-    parser.add_argument('--port', type=int, default=5002)
-    parser.add_argument('--debug', action='store_true')
-    args = parser.parse_args()
+    #parser.add_argument('--host', type=str, default='127.0.0.1')
+    #parser.add_argument('--port', type=int, default=5002)
+    #parser.add_argument('--debug', action='store_true')
+    #args = parser.parse_args()
 
-    app.run(host=args.host, port=args.port, debug=args.debug)
+    #app.run(host=args.host, port=args.port, debug=args.debug)
+    app.run()
